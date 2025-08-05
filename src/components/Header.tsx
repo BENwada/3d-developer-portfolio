6e767f3b526cd6d0dd81029d685cf9355c2ba8c0
BENwada <domo0103wada@gmail.com>
2025-08-06 08:49:45 +0900

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import { motion } from "motion/react";
+const navmenu = ["Home", "About", "Projects", "Experience", "Contact"];
+const Header = () => {
+  return (
+    <header className="absolute w-full z-50 transition-all duration-300">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 md:h-20">
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{
+            type: "spring",
+            stiffness: 100,
+            damping: 25,
+            delay: 0.3,
+            duration: 1.2,
+          }}
+          className="flex items-center"
+        >
+          <div className="h-10 w-10 rounded-xl bg-gradient-to-r from-gray-500 to-gray-100 flex items-center justify-center text-purple-600 font-bold text-xl mr-3 ">
+            M
+          </div>
+          <span className="text-xl font-bold bg-gradient-to-r from-gray-300 to-gray-100 bg-clip-text text-transparent">
+            MiladiCode
+          </span>
+        </motion.div>
+
+        {/* Desctop Navigation */}
+        <div className="lg:flex hidden space-x-8">
+          {navmenu.map((item, index) => (
+            <a
+              key={index}
+              className="relative text-gray-800 dark:text-gray-200 hover:violet-600 dark:hover:text-violet-400 font-medium transition-colors duration-300 group"
+              href="#"
+            >
+              {item}
+            </a>
+          ))}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
