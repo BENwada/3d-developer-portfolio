7a04f08178d2427d9a6033c81eef2b7a5eebe727
BENwada <domo0103wada@gmail.com>
2025-08-13 17:07:19 +0900

    projects section page add

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import CustomCursor from "./components/CustomCursor";
 import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
 
 const App = () => {
+  useEffect(() => {
+    // Register ScrollTrigger plugin
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Refresh ScrollTrigger when the page is fully Loaded
+    ScrollTrigger.refresh();
+
+    // Clean up ScrollTrigger on component unmount
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, []);
+
   return (
     <>
       <Header />
       <HeroSection />
       <CustomCursor />
       <AboutSection />
+      <ProjectsSection />
     </>
   );
 };
